c71ff964e1c1416bcc7a681712684c40bed3131c
ku5h4gr4 <kushagra.singh162@gmail.com>
2025-11-03 22:50:25 +0530

    adding dtos for order and payment

---
+++ src/main/java/com/ecommerce/project/payload/OrderDTO.java
@@ -0,0 +1,23 @@
+package com.ecommerce.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+    private Long orderId;
+    private String email;
+    List<OrderItemDTO> orderItem;
+    private PaymentDTO payment;
+    private LocalDate orderDate;
+    private Double totalAmount;
+    private String orderStatus;
+    private Long addressId;
+}
