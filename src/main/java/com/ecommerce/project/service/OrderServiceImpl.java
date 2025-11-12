68e0a23ed95063ac75f27711b3503dc6d5218783
ku5h4gr4 <kushagra.singh162@gmail.com>
2025-11-12 23:39:46 +0530

    implement orderService Layer logic

--- src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
@@ -1,7 +1,80 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.ApiException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.*;
+import com.ecommerce.project.payload.OrderDTO;
+import com.ecommerce.project.repositories.*;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Override
+    @Transactional
+    public OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage) {
+
+//        GETTING THE USER CART
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        if (cart == null){
+            throw new ResourceNotFoundException("Cart","email",emailId);
+        }
+
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address","addressId",addressId));
+
+        Order order = new Order();
+        order.setEmail(emailId);
+        order.setOrderDate(LocalDate.now());
+        order.setTotalAmount(cart.getTotalPrice());
+        order.setAddress(address);
+
+        Payment payment = new Payment(paymentMethod, pgPaymentId, pgStatus, pgResponseMessage, pgName);
+        payment.setOrder(order);
+        payment = paymentRepository.save(payment);
+
+        order.setPayment(payment);
+        Order savedOrder = orderRepository.save(order);
+
+        List<CartItem> cartItems = cart.getCartItems();
+        if(cartItems.isEmpty()){
+            throw new ApiException("Cart is Empty");
+        }
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        for(CartItem cartItem: cartItems){
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(cartItem.getProduct());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setDiscount(cartItem.getDiscount());
+            orderItem.setOrderedProductPrice(cartItem.getProductPrice());
+            orderItem.setOrder(savedOrder);
+            orderItems.add(orderItem);
+        }
+
+        orderItems = orderItemRepository.saveAll(orderItems);
+
+        return null;
+    }
 }
